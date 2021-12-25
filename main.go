4ea3b73eb89c1b471370e963bcd19dde3cb5db75
vivek kumar sahu <vivekkumarsahu650@gmail.com>
2021-12-25 10:56:18 +0530

    Created simple news web app

---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+	"math"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/freshman-tech/news-demo-starter-files/news"
+	"github.com/joho/godotenv"
+)
+
+type Search struct {
+	Query      string
+	NextPage   int
+	TotalPages int
+	Results    *news.Results
+}
+
+var newsapi *news.Client
+
+// tpl is a package level variable that points to a template definition from the provided files
+
+//template.ParseFiles is wrapped with template.Must so that the code panics if an error is obtained while parsing the template file
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+func searchHandler(newsapi *news.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse((r.URL.String()))
+		fmt.Println("r.URL.String(): ", r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		params := u.Query()
+		fmt.Println("param: ", params)
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
+
+		fmt.Println("Search Query is: ", searchQuery)
+		fmt.Println("Page is: ", page)
+
+		results, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println("%+v", results)
+
+		nextPage, err := strconv.Atoi(page)
+		fmt.Println("nextPage: ", nextPage)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		search := &Search{
+			Query:      searchQuery,
+			NextPage:   nextPage,
+			TotalPages: int(math.Ceil(float64(results.TotalResults) / float64(newsapi.PageSize))),
+			Results:    results,
+		}
+
+		buf := &bytes.Buffer{}
+		err = tpl.Execute(buf, search)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		buf.WriteTo(w)
+	}
+}
+
+// w --> it send response to the client request
+// r --> it accept request from client
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("<h1>Hi vivek!! This is your first web page..!</h1>"))
+	// w.Write([]byte("<h2>Change in the Port nubmer..</h2>"))
+	// w.Write([]byte("News App API..."))
+
+	buf := &bytes.Buffer{}
+	err := tpl.Execute(buf, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	buf.WriteTo(w)
+	// two arguments: where we want to write the output to, and the data we want to pass to the template.
+	// tpl.Execute(w, nil)
+}
+
+func main() {
+	// Load() --> reads the .env file and loads the set variables into the environment
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error in loading .env files..")
+	}
+	fmt.Println("PORT is: ", os.Getenv("PORT"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Env: apiKey must be set")
+	}
+
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsapi = news.NewClient(myClient, apiKey, 20)
+
+	fs := http.FileServer(http.Dir("assets"))
+	// create an HTTP request multiplexer which is subsequently assigned to the mux variable
+	// calls the associated handler for the pattern whenever a match is found.
+	mux := http.NewServeMux()
+
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/search", searchHandler(newsapi))
+	// mux.HandleFunc("/search", searchHandler)
+	mux.HandleFunc("/", indexHandler)
+
+	// starts the server on the port
+	http.ListenAndServe(":"+port, mux)
+}
---
+++ news/news.go
@@ -0,0 +1,74 @@
+package news
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Article struct {
+	Source struct {
+		ID   interface{} `json:"id"`
+		Name string      `json:"name"`
+	} `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
+}
+
+type Results struct {
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
+}
+
+type Client struct {
+	// httpClient --> client that should be used to make requests
+	http *http.Client
+
+	// apiKey field holds the API key
+	key string
+
+	// number of results to return per page (maximum of 100)
+	PageSize int
+}
+
+// NewClient() function creates and returns a new Client instance for making requests to the News API.
+func NewClient(httpClient *http.Client, key string, pageSize int) *Client {
+	if pageSize > 100 {
+		pageSize = 100
+	}
+	return &Client{httpClient, key, pageSize}
+}
+
+func (c *Client) FetchEverything(query, page string) (*Results, error) {
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(query), c.PageSize, page, c.key)
+	fmt.Println("endpoint: ", endpoint)
+	resp, err := c.http.Get(endpoint)
+	fmt.Println("response: ", resp)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println("body: ", string(body))
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(string(body))
+	}
+
+	res := &Results{}
+	return res, json.Unmarshal(body, res)
+}
